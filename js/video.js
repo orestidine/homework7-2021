ac4e912e1d1cbfe333600557b3d1a201c1a73ab3
orestidine <89855228+orestidine@users.noreply.github.com>
2021-11-18 08:30:59 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,11 +1,58 @@
-var video;
+var video = document.querySelector("#player1");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
-
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	volume.innerHTML = "100%";
+});
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down" + video.playbackRate);
+	video.playbackRate *=.95;
+});
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed Up" + video.playbackRate);
+	video.playbackRate /=.95;
+});
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log(this)
+	if (video.currentTime < video.duration - 15){
+		video.currentTime += 15;
+}
+	else {
+		video.currentTime = 0;
+		console.log("Skip Ahead");
+	}
+		video.play();
+		console.log(video.duration);
+		console.log("currentLocation"+video.currentTime);
+});
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted) {
+		video.muted = false;
+		this.innerHTML = "Mute";
+	} else{
+		 	video.muted = true;
+			this.innerHTML = "Unmute";
+		}
+});
+document.querySelector("#slider").addEventListener("change", function() {
+	video.volume = this.value / 100;
+	volume.innerHTML = this.value + "%";
+	console.log(this.value);
+	console.log(video.volume);
+});
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+});
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool")
+});
